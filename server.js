59d9ae2b25b50a047c9255a0158907c0685efa1a
jmanwel <new.juang@gmail.com>
2024-01-19 16:13:37 -0300

    [ADD] uploading files

--- server.js
+++ server.js
@@ -1,12 +1,43 @@
 const http = require('http');
 const port = 3000;
 const hostname = 'localhost';
+const fs = require("fs");
+
+
 
 const server = http.createServer((req,res)=>{
-    res.setHeader("Content-type", "text/plain");
-    res.write("Welcome to jmg")
-    console.log("request url", req.url)
-    console.log("request method", req.method)
+    res.setHeader("Content-type", "text/html");
+
+    let route = "./views/";
+    switch(req.url){
+        case "/":
+            route += "index.html";
+            res.statusCode = 200;
+            break;
+        case "/contact":
+            route += "contact.html";
+            res.statusCode = 200;
+            break;
+        case "/contact-us":
+            res.statusCode = 301;
+            res.setHeader("Location", "/contact");
+            res.end();
+            break;
+        default:
+            route += "404.html";
+            res.statusCode = 404;
+            break;
+    }
+
+    fs.readFile(route, (err,data)=>{
+        if (err) {
+            console.log(err)
+        }else {
+            res.write(data)
+        }
+        res.end();
+    })
+
 
 })
 
